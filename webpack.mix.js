e6bb66512e5f85341780209e1b737b155e1d04c0
Juan Menéndez <juan.menendez.buitrago@gmail.com>
2017-05-14 15:24:15 +0200

    add tasks

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,22 @@ const { mix } = require('laravel-mix');
  |
  */
 
+// VENDOR STUFF
+mix.copy('node_modules/font-awesome/css/font-awesome.min.css', 'public/css/vendor/font-awesome/css/')
+   .copy('node_modules/font-awesome/fonts/*', 'public/css/vendor/font-awesome/fonts/')
+   .copy('node_modules/proxima-nova-fonts/webfonts/*', 'public/css/vendor/bootstrap/webfonts/')
+   .copy('node_modules/bootstrap-sass/assets/fonts/bootstrap/*', 'public/css/vendor/bootstrap/fonts/')
+   .copy('node_modules/jquery/dist/jquery.min.js', 'public/js/vendor/jquery/')
+   .copy('node_modules/js-cookie/src/js.cookie.js', 'public/js/vendor/js-cookie/')
+   .copy('node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js', 'public/js/vendor/bootstrap/')
+   .copy('node_modules/dropzone/dist/*.js', 'public/js/vendor/dropzone/')
+   .copy('node_modules/dropzone/dist/*.css', 'public/css/vendor/dropzone/')
+   .copy('resources/assets/js/jquery*','public/js/')
+   .copy('resources/assets/img/*', 'public/img/');	
+
+// OWN STUFF   
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .sass('resources/assets/sass/tfg-bootstrap.scss', 'public/css/vendor/bootstrap/css/bootstrap.css').options({processCssUrls: false});
+
+mix.version();
